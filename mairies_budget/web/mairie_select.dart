0ecde276ae2e15a446145dabce54f9c5f9a3404a
florent <florent@stagetour>
2014-02-12 15:16:35 +0100

    Add select bos as polymer element

---
+++ mairies_budget/web/mairie_select.dart
@@ -0,0 +1,22 @@
+import 'package:polymer/polymer.dart';
+import 'package:logging_handlers/logging_handlers_shared.dart';
+
+    @CustomTag( 'mairie-select' )
+    class MairieSelect extends PolymerElement 
+    {
+      
+      
+      @observable int selected = 0;
+       
+      final List<String> mairiesList = toObservable(
+          ['Paris', 'Nanterre', 'RueilMalmaison', 'Marseille', 'Versailles','Autres...'  ]);
+      @observable String value = 'Paris';
+      @observable String texte = 'DuTextePourle fun';
+      MairieSelect.created() : super.created();
+     
+      void showForm() {
+        this.fire('mairie-select');
+      }
+     
+    
+    }
